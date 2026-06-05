103b8f450e2c653bf4fc68898bbc49e665a50905
micaelbush-commits <micaelbush@gmail.com>
2026-06-05 18:19:43 +0300

    Commit to main

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// КОНСТАНТЫ - замените на свои значения!
+// КОНСТАНТЫ - замените не свои значения!
 const SUPABASE_URL='https://jvkydzhziwjejohbrtkw.supabase.co';
 const SUPABASE_KEY='sb_publishable_iAGSy8_mZ7nZClH8nQDgNA_n4IbzRdA';
 // Проверяем, что ключи заданы
