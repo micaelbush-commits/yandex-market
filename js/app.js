07b96d9b3bce7a70063615a3c5a6344f5e1eacff
micaelbush-commits <micaelbush@gmail.com>
2026-06-05 18:02:51 +0300

    Fix Supabase keys

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// КОНСТАНТЫ - замните на свои значения!
+// КОНСТАНТЫ - замените на свои значения!
 const SUPABASE_URL = 'https://jvkydzhiwjejohbrtkw.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_iAGSy8_mZ7nZClH8nQDgNA_n4IbzRdA';
 // Проверяем, что ключи заданы
